1f3ba25eaf5e45667ddfff73d18e241fda083189
bruce_zhang <bruce_zhang@trendmicro.com>
2021-12-02 17:22:07 +0800

    update webpack config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,5 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, '../src/index.ts')
+  entry: path.resolve(__dirname, '../src/index.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'flood.js',
+    path: path.join(__dirname, '../dist')
+  }
 }
