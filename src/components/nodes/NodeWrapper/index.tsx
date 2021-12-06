2c79bd55c4c9782c2f15cd32ab784f080d5bd1c5
bruce_zhang <bruce_zhang@trendmicro.com>
2021-12-06 16:05:31 +0800

    NodeWrapper use useDrag

--- src/components/nodes/NodeWrapper/index.tsx
+++ src/components/nodes/NodeWrapper/index.tsx
@@ -1,4 +1,5 @@
 import { useState, useCallback, useMemo, useEffect, ReactNode, useRef } from 'react'
+import useDrag from 'src/hooks/useDrag';
 
 import styles from './styles.module.scss';
 
@@ -12,22 +13,19 @@ function NodeWrapper(props: NodeWrapperProps) {
   const { children, onNodeMove } = props;
   // ref
   const node = useRef(null);
+  // custom hooks
+  const {
+    mouseDown,
+    mouseDeltaPosition,
+    handleMouseDown,
+    handleMouseMove,
+    handleMouseUp
+  }=useDrag();
 
   // states
-  const [mouseDown, setMouseDown] = useState(false);
-  const [mouseStartPosition, setMouseStartPosition] = useState({ x: 0, y: 0 });
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const [nodeLastPosition, setNodeLastPosition] = useState({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
 
   // cached states
-  const mouseDeltaPosition = useMemo(() => {
-    const result = {
-      x: mousePosition.x - mouseStartPosition.x,
-      y: mousePosition.y - mouseStartPosition.y,
-    }
-    return result;
-  }, [mouseStartPosition, mousePosition]);
-
   const nodePosition = useMemo(() => {
     return {
       x: nodeLastPosition.x + mouseDeltaPosition.x,
@@ -35,51 +33,25 @@ function NodeWrapper(props: NodeWrapperProps) {
     }
   }, [nodeLastPosition, mouseDeltaPosition]);
 
-
   // mouse actions
-  const handleMouseDown = useCallback((e) => {
-    e.stopPropagation();
-    setMouseDown(true);
-    const { clientX, clientY } = e;
-    setMouseStartPosition({
-      x: clientX,
-      y: clientY
-    });
-    setMousePosition({
-      x: clientX,
-      y: clientY
-    });
-  }, []);
-
-  const handleMouseMove = useCallback((e) => {
-    if (!mouseDown) return;
-    const { clientX, clientY } = e;
-    setMousePosition({
-      x: clientX,
-      y: clientY
-    });
-  }, [mouseDown]);
-
-  const handleMouseUp = useCallback(() => {
+  const selfHandleMouseUp = useCallback((e) => {
+    handleMouseUp(e);
     if (!node?.current) return;
     // lastest nodePosition states won't be fetched, use data-x instead
     const nodeEl = node.current as HTMLElement;
     const lastNodeX = nodeEl.dataset.x ? parseInt(nodeEl.dataset.x) : 0;
     const lastNodeY = nodeEl.dataset.y ? parseInt(nodeEl.dataset.y) : 0;
-    setMouseDown(false);
     setNodeLastPosition({ x: lastNodeX, y: lastNodeY });
-    setMouseStartPosition({ x: 0, y: 0 });
-    setMousePosition({ x: 0, y: 0 });
   }, [nodePosition]);
 
   // effects
   useEffect(() => {
     if (mouseDown) {
       window.addEventListener('mousemove', handleMouseMove);
-      window.addEventListener('mouseup', handleMouseUp);
+      window.addEventListener('mouseup', selfHandleMouseUp);
       return () => {
         window.removeEventListener('mousemove', handleMouseMove);
-        window.removeEventListener('mouseup', handleMouseUp);
+        window.removeEventListener('mouseup', selfHandleMouseUp);
       }
     }
   }, [mouseDown]);
