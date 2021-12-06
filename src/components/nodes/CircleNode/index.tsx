09f02a7692d49ba3afa915db73672bb55dac005e
bruce_zhang <bruce_zhang@trendmicro.com>
2021-12-06 14:36:47 +0800

    Circle node add anchors

--- src/components/nodes/CircleNode/index.tsx
+++ src/components/nodes/CircleNode/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
 import NodeWrapper from '../NodeWrapper';
 import Circle from 'src/components/shapes/Circle';
+import Anchor from 'src/components/Anchor';
 
-export default function RectNode() {
+export default function CircleNode() {
   return (
     <NodeWrapper>
       <Circle />
+      <Anchor type="in" x={-49} y={-10} />
+      <Anchor type="out" cx={45} cy={0} />
     </NodeWrapper>
   )
 }
